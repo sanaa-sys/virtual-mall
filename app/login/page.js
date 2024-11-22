6faa1bd2e850bf917d9940e9579fc126d16e3877
Areeba <afsbibi@gmail.com>
2024-11-22 18:28:28 +0500

    font size

--- app/login/page.js
+++ app/login/page.js
@@ -39,8 +39,8 @@ export default function Login() {
         <div className="flex items-center justify-center py-12 min-h-screen">
           <div className="mx-auto grid w-[350px] gap-6">
             <div className="grid gap-2 text-center">
-              <h1 className="text-3xl font-bold">Welcome to Virtual Mall</h1>
-              <h1 className="text-3xl font-bold">Login</h1>
+              <h1 className="text-xl font-bold">Welcome to Virtual Mall</h1>
+              <h1 className="text-xl font-bold">Login</h1>
               <p className="text-muted-foreground">
                 Enter your email below to login to your account
               </p>
