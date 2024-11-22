c91a8b6746e57349e70b08f5b4ba3d86f16483e4
Areeba <afsbibi@gmail.com>
2024-11-22 18:24:20 +0500

    signin

--- app/page.js
+++ app/page.js
@@ -64,7 +64,7 @@ export default function Home() {
       <div className="flex items-center justify-center py-12">
         <div className="mx-auto grid w-[350px] gap-6">
           <div className="grid gap-2 text-center">
-            <h1 className="text-3xl font-bold">Welcome to Virtual Mall</h1>
+            <h1 className="text-xl font-bold">Welcome to Virtual Mall</h1>
             <h1 className="text-3xl font-bold">Sign Up</h1>
             <p className="text-balance text-muted-foreground">
               Enter your email below to create an account
